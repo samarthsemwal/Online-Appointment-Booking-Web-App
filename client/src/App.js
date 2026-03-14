b81ccbcd48e8ee7ac560477b2d7d4647d1ba1e6b
samarthsemwal <109367065+samarthsemwal@users.noreply.github.com>
2026-03-14 12:16:44 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,21 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "../pages/Home";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 function App() {
+  return (
+    <BrowserRouter>
 
-return (
+      <Navbar />
 
-<BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
 
-<Routes>
-
-<Route path="/" element={<Home />} />
-
-</Routes>
-
-</BrowserRouter>
-
-);
-
+    </BrowserRouter>
+  );
 }
 
 export default App;
