7a86095410507cd50cc1a7e9d59a0a784db0eb27
samarthsemwal <109367065+samarthsemwal@users.noreply.github.com>
2026-03-16 22:04:37 +0530

    Refactor Navbar to use Link and add buttons
    Updated Navbar component to use Link from react-router-dom for navigation and added buttons for Login and Register.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,23 +1,48 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
-      <div className="container">
 
-        <a className="navbar-brand fw-bold" href="/">
-          DocApp
-        </a>
+return(
 
-        <div>
-          <a className="nav-link d-inline me-3" href="/">Home</a>
-          <a className="nav-link d-inline me-3" href="/doctors">Doctors</a>
-          <a className="nav-link d-inline" href="/login">Login</a>
-        </div>
+<nav className="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
 
-      </div>
-    </nav>
-  );
+<div className="container">
+
+<Link className="navbar-brand fw-bold" to="/">
+DocApp
+</Link>
+
+<div>
+
+<Link className="nav-link d-inline me-3" to="/">
+Home
+</Link>
+
+<Link className="nav-link d-inline me-3" to="/doctors">
+Doctors
+</Link>
+
+<Link className="nav-link d-inline me-3" to="/appointments">
+My Appointments
+</Link>
+
+<button className="btn btn-outline-primary me-2">
+Login
+</button>
+
+<button className="btn btn-primary">
+Register
+</button>
+
+</div>
+
+</div>
+
+</nav>
+
+)
+
 }
 
 export default Navbar;
