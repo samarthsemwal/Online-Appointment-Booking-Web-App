10101b4fa495694a7d171aef0ab9d702fcb46bb6
samarthsemwal <109367065+samarthsemwal@users.noreply.github.com>
2026-03-30 09:13:25 +0530

    Add doctorId and patientId fields to Appointment schema

--- server/models/Appointment.js
+++ server/models/Appointment.js
@@ -3,12 +3,17 @@ const mongoose = require("mongoose");
 const AppointmentSchema = new mongoose.Schema({
 
 doctor:String,
+doctorId:String,
 speciality:String,
 location:String,
+
 patient:String,
 email:String,
+
 date:String,
-time:String
+time:String,
+
+patientId:String
 
 });
 
