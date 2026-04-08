c42fc07bfcdf637daa6e258473df0263cf3618a4
samarthsemwal <109367065+samarthsemwal@users.noreply.github.com>
2026-04-08 06:54:50 +0530

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,79 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import Navbar from "./components/Navbar";
+
 import Home from "./pages/Home";
 import Doctors from "./pages/Doctors";
+import DoctorDetails from "./pages/DoctorDetails";
+import Appointments from "./pages/Appointments";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
-function App() {
-  return (
-    <BrowserRouter>
+// New Pages
+import DoctorDashboard from "./pages/DoctorDashboard";
+import ChatVideoCall from "./pages/ChatVideoCall";
+import HeartDisease from "./pages/HeartDisease";
 
-      <Navbar />
+import ProtectedRoute from "./components/ProtectedRoute";
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/doctors" element={<Doctors />} />
-      </Routes>
+function App(){
 
-    </BrowserRouter>
-  );
+return(
+
+<BrowserRouter>
+
+<Navbar/>
+<ToastContainer position="top-right" autoClose={3000} hideProgressBar={false} />
+
+<Routes>
+
+<Route path="/" element={<Home/>} />
+<Route path="/doctors" element={<Doctors/>} />
+<Route path="/login" element={<Login/>} />
+<Route path="/register" element={<Register/>} />
+<Route path="/heart-disease" element={<HeartDisease/>} />
+
+<Route
+path="/appointments"
+element={
+<ProtectedRoute>
+<Appointments/>
+</ProtectedRoute>
+}
+/>
+
+<Route
+path="/doctor/:id"
+element={<DoctorDetails/>}
+/>
+
+<Route
+path="/doctor-dashboard"
+element={
+<ProtectedRoute>
+<DoctorDashboard/>
+</ProtectedRoute>
+}
+/>
+
+<Route
+path="/communication/:appointmentId"
+element={
+<ProtectedRoute>
+<ChatVideoCall/>
+</ProtectedRoute>
+}
+/>
+
+</Routes>
+
+</BrowserRouter>
+
+)
+
 }
 
 export default App;
